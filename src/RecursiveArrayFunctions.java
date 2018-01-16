22187fe0e47e80e8eb54adeb8e88d3eec8b6bcc3
Rixium <Gamerpotty@gmail.com>
2018-01-16 12:29:41 +0000

    Refactored the functions.

--- src/RecursiveArrayFunctions.java
+++ src/RecursiveArrayFunctions.java
@@ -7,7 +7,7 @@ public class RecursiveArrayFunctions {
 	
 	public static void main(String[] args) {
 		// An array of our numbers.
-		int[] numbers = { 25, 10, 12, 2, 28 };
+		int[] numbers = { 25, 10, 43, 2, 21 };
 		
 		// Output the sum of the values.
 		System.out.println(sum(0, numbers));
@@ -21,32 +21,19 @@ public class RecursiveArrayFunctions {
 	
 	// Adds each number in an array to each other recursively.
 	public static int sum(int index, int[] numbers) {
-		if(index == numbers.length - 1) {
-			return numbers[index];
-		} else {
-			return numbers[index] + sum(++index, numbers);
-		}
+		return index == numbers.length - 1 ? numbers[index] : numbers[index] + sum(++index, numbers);
 	}
 	
 	// Counts the amount of items in an array recursively.
 	public static int count(int index, int[] numbers) {
-		if(index == numbers.length) {
-			return index;
-		} else {
-			return count(++index, numbers);
-		}
+		return index == numbers.length ? index : count(++index, numbers);
 	}
 	
 	// Recursively searches through an array for the biggest value.
 	public static int maxNum(int index, int[] numbers, int maxNum) {
-		if(index == numbers.length) {
+		if(index == numbers.length)
 			return maxNum;
-		} else {
-			if(numbers[index] > maxNum) {
-				return maxNum(index + 1, numbers, numbers[index]);
-			} else {
-				return maxNum(index + 1, numbers, maxNum);
-			}
-		}
+		else
+			return numbers[index] > maxNum ? maxNum(index + 1, numbers, numbers[index]) :  maxNum(index + 1, numbers, maxNum);
 	}
 }
